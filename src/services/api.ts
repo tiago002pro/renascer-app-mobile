a31b56fa502051d1b90823b71c2c8f32f0090296
tiago002pro <tiagobarbosa02@outlook.com>
2024-03-06 02:41:07 -0300

    Permissão de internet na app

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,8 +1,8 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://3.87.226.191:8080",
-  // baseURL: "http://10.0.0.107:8080",
+  baseURL: "http://54.224.48.116:8080",
+  // baseURL: "http://10.0.0.106:8080",
   headers: {
     "Content-Type": "application/json",
     "Accept": "application/json",
