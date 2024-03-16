cb7c7f7f1e1e0edd4f54af34b297089d75b05ef0
tiago002pro <tiagobarbosa02@outlook.com>
2024-03-16 15:40:58 -0300

    Batendo na api em produção

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://54.224.48.116:8080",
+  baseURL: "http://54.89.196.1:8080/",
   // baseURL: "http://10.0.0.106:8080",
   headers: {
     "Content-Type": "application/json",
