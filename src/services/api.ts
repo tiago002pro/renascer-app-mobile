92111d70794e6fbc77f315e2dea15492e29ddc3c
tiago002pro <tiagobarbosa02@outlook.com>
2024-05-24 23:10:05 -0300

    Expo-doctor

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,8 +1,8 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://44.204.26.91:8080/",
-  // baseURL: "http://10.0.0.111:8080",
+  // baseURL: "http://44.204.26.91:8080/",
+  baseURL: "http://10.0.0.100:8080",
   headers: {
     "Content-Type": "application/json",
     "Accept": "application/json",
