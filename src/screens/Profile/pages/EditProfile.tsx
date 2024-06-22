78e9f63664c809f98ea83e74c23e149dd16b947e
tiago002pro <tiagobarbosa02@outlook.com>
2024-06-22 10:18:04 -0300

    Clean code

--- src/screens/Profile/pages/EditProfile.tsx
+++ src/screens/Profile/pages/EditProfile.tsx
@@ -1,84 +1,26 @@
-import { useEffect, useRef, useState } from "react";
+import { useRef, useState } from "react";
 import { Animated, StyleSheet, useWindowDimensions } from "react-native";
 import { FlatList, View } from "native-base";
-import { showMessage } from "react-native-flash-message";
 import { ActivityIndicator } from "react-native-paper";
-
-import { useAuth } from "../../../contexts/auth";
-import UserService from "../service/UserService";
-import PersonService from "../service/PersonService";
-
 import { OnboardingItem } from "../components/OnboardingItem";
-import { useNavigation } from "@react-navigation/native";
 import { Paginator } from "../components/Paginator";
-
 import { THEME } from "../../../styles/theme";
 
 export function EditProfile({ route }:any) {
-  const navigation:any = useNavigation();
   const { data } = route.params; 
-  const { user } = useAuth() as any;
   const { width, height } = useWindowDimensions();
-
-  const [person, setPerson] = useState(null) as any;
-  const [address, setAddress] = useState(null);
-  const [currentIdex, setCurrentIdex] = useState(0);
-  const [loading, setLoading] = useState(false);
-
+  const [person, setPerson] = useState<any>(null);
   const scrollx:any = useRef(new Animated.Value(0)).current;
   const slidesRef:any = useRef(null);
 
-  const viewableItemsChanged = useRef(({ viewableItems }:any) => {
-    setCurrentIdex(viewableItems[0].index)
-  }).current;
-
-  const viewConfig = useRef({ viewAreaCoveragePercentThreshold: 50 }).current;
-
-  const scrollTo = () => {
-    setTimeout(() => {
-      if (currentIdex < data.length - 1) {
-        slidesRef.current.scrollToIndex({ index: currentIdex + 1 });
-      }
-    })
-  }
-
-  async function save() {
-    person.address = address
-
-    await PersonService.update(person).then(() => {
-      showMessage({ message: "Salvo com sucesso", type: "success"})
-      navigation.navigate('Settings')
-    }).catch(() => {
-      showMessage({ message: "Algo deu errado", type: "danger" })
-    })
-  }
-
-  async function getUser() {
-    const data = await UserService.loadUser(parseInt(user.id))
-    
-    setPerson(data?.person)
-    setAddress(data?.person?.address || {})
-  }
-
-  useEffect(() => {
-    async function onInit() {
-      getUser()
-
-      setLoading(true)
-      setTimeout(() => { setLoading(false) }, 1000);
-    }
-    
-    onInit()
-  }, [navigation])
-
   return (
     <View style={[styles.container, { width }]}>
-      <View style={[styles.loading, { width, height }]} flex={1} position={'absolute'} opacity={loading ? 1 : 0}>
+      <View style={[styles.loading, { width, height }]} flex={1} position={'absolute'}>
         <ActivityIndicator size={"large"} color={THEME.colors.primary} />
       </View>
 
       <Paginator data={data} scrollx={scrollx} />
-      <View style={styles.body} opacity={loading ? 0 : 1}>
+      <View style={styles.body}>
         <FlatList
           data={data}
           renderItem={
@@ -98,8 +40,6 @@ export function EditProfile({ route }:any) {
             useNativeDriver: false,
           })}
           scrollEventThrottle={32}
-          onViewableItemsChanged={viewableItemsChanged}
-          viewabilityConfig={viewConfig}
           ref={slidesRef}
         />
       </View>
