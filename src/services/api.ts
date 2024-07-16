d9124b19c68de201bc6f211f527d83966d72c88e
tiago002pro <tiagobarbosa02@outlook.com>
2024-07-15 23:18:11 -0300

    Produção

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,8 +1,8 @@
 import axios from "axios";
 
 const api = axios.create({
-  // baseURL: "http://195.200.0.62:8080/",
-  baseURL: "http://10.0.0.105:8080",
+  baseURL: "http://195.200.0.62:8080/",
+  // baseURL: "http://10.0.0.107:8080",
   headers: {
     "Content-Type": "application/json",
     "Accept": "application/json",
